699aa2cbb9337589b14123da8763a4fa6b0a8ae7
Jack Lewis <jalew@microsoft.com>
2021-09-30 15:44:07 +0100

    folder restructure

--- README.md
+++ README.md
@@ -5,6 +5,31 @@ A Microsoft Teams app that will be used by the Human Resources department within
 
 The lab file is attached, but it would require extra steps on your part to setup your own O365 Tenant and pre-create Teams, Channels, Personas. Please use it for your reference.
 
+# Microsoft Teams Talent Management Sample App in .NET/C#
+
+This app simulates connection to a recruiting and talent management system and allows users and teams to create, manage and search positions and candidates. The content is randomly generated to simulate what you can do with Teams. The full source code is provided, along with the app package and manifest that you can use to side load and test in your Teams environment.
+
+This .NET/C# sample only shows the Bot, Tab, and Messaging Extension portion of the sample.
+
+**For more information on developing apps for Microsoft Teams, please review the Microsoft Teams [developer documentation](https://msdn.microsoft.com/en-us/microsoft-teams/index).**
+
+## Prerequisites
+The minimum prerequisites to run this sample are:
+* The latest update of Visual Studio. You can download the community version [here](http://www.visualstudio.com) for free.
+* An Office 365 account with access to Microsoft Teams, with [sideloading enabled](https://msdn.microsoft.com/en-us/microsoft-teams/setup).
+* Ngrok, a tool to run your app service locally within Teams, available [here](https://ngrok.com/).
+
+>**Note**: some features in the sample require that you [enable Public Developer Preview mode](https://msdn.microsoft.com/en-us/microsoft-teams/publicpreview) in Microsoft Teams.
+
+For more information about how to configure and test our samples, see [Sample applications for the Microsoft Teams Developer Platform](https://msdn.microsoft.com/en-us/microsoft-teams/samples).
+
+
+## More Information
+For more information about getting started with Teams, please review the following resources:
+- Review [Getting Started with Teams](https://msdn.microsoft.com/en-us/microsoft-teams/setup)
+- Review [Getting Started with Bot Framework](https://docs.microsoft.com/en-us/bot-framework/bot-builder-overview-getstarted)
+- Review [Testing your bot with Teams](https://msdn.microsoft.com/en-us/microsoft-teams/botsadd)
+
 # Contributing
 
 This project welcomes contributions and suggestions.  Most contributions require you to agree to a
---
+++ src/TeamsTalentMgmtAppV4/Controllers/ClientApiController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Mime;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using TeamsTalentMgmtAppV4.Models;
+using TeamTalentMgmtApp.Shared.Models.DatabaseContext;
+using TeamTalentMgmtApp.Shared.Models.Dto;
+using TeamTalentMgmtApp.Shared.Services.Interfaces;
+
+namespace TeamsTalentMgmtAppV4.Controllers
+{
+    [ApiController]
+    public class ClientApiController : ControllerBase
+    {
+        private readonly AppSettings _appSettings;
+        private readonly IMapper _mapper;
+        private readonly ICandidateService _candidateService;
+        private readonly IPositionService _positionService;
+
+        public ClientApiController(
+            IMapper mapper,
+            IOptions<AppSettings> appSettings,
+            ICandidateService candidateService,
+            IPositionService positionService)
+        {
+            _appSettings = appSettings.Value;
+            _candidateService = candidateService;
+            _positionService = positionService;
+            _mapper = mapper;
+        }
+
+        //todoscott:add controller access token logic....
+
+        [HttpGet]
+        [Route("api/app")]
+        public ActionResult Get() => Ok(new
+        {
+            appId = _appSettings.TeamsAppId,
+            botId = _appSettings.MicrosoftAppId
+        });
+
+        [HttpGet]
+        [Route("api/candidates/{id}")]
+        public async Task<ActionResult<CandidateDto>> GetCandidateById(int id, CancellationToken cancellationToken)
+        {
+            var candidate = await _candidateService.GetById(id, cancellationToken);
+            return Ok(_mapper.Map<CandidateDto>(candidate));
+        }
+
+        [HttpGet]
+        [Route("api/candidates/{id}/profilePicture")]
+        public async Task<HttpResponseMessage> GetCandidateImageById(int id, CancellationToken cancellationToken)
+        {
+            var user = _mapper.Map<CandidateDto>(await _candidateService.GetById(id, cancellationToken));
+            using (var ms = new MemoryStream(Convert.FromBase64String(user.ProfilePictureDataOnly)))
+            {
+                var result = new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new ByteArrayContent(ms.ToArray())
+                };
+                result.Content.Headers.ContentType = new MediaTypeHeaderValue(MediaTypeNames.Image.Jpeg);
+                return result;
+            }
+        }
+
+        [HttpPut]
+        [Route("api/candidates/")]
+        public async Task<ActionResult> UpdateCandidateStage([FromBody] Candidate candidate, CancellationToken cancellationToken)
+        {
+            await _candidateService.UpdateCandidateStage(candidate.CandidateId, candidate.Stage, cancellationToken);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("api/positions")]
+        public async Task<ActionResult> GetAllPositions(CancellationToken cancellationToken)
+        {
+            var positions = await _positionService.GetAllPositions(cancellationToken);
+            return Ok(_mapper.Map<List<PositionDto>>(positions));
+        }
+
+        [HttpGet]
+        [Route("api/positions/{id}")]
+        public async Task<ActionResult<PositionDto>> GetPositionById(int id, CancellationToken cancellationToken)
+        {
+            var position = await _positionService.GetById(id, cancellationToken);
+            return Ok(_mapper.Map<PositionDto>(position));
+        }
+
+        [HttpGet]
+        [Route("api/recruiters/{alias}/positions")]
+        public async Task<ActionResult<PositionDto>> GetPositionsByRecruiterAlias(string alias, CancellationToken cancellationToken)
+        {
+            var positions = await _positionService.GetOpenPositions(alias, cancellationToken);
+            return Ok(_mapper.Map<List<PositionDto>>(positions));
+        }
+
+        [HttpGet]
+        [Route("api/positions/open")]
+        public async Task<ActionResult<PositionDto>> GetAllOpenPositions(CancellationToken cancellationToken)
+        {
+            var positions = await _positionService.GetOpenPositions(string.Empty, cancellationToken);
+            return Ok(_mapper.Map<List<PositionDto>>(positions));
+        }
+    }
+}
