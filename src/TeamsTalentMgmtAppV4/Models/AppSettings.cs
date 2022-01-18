dfe00027a10b4c687edae40b45bb1c5a6adc776e
Scott Perham <scott.perham@microsoft.com>
2022-01-18 15:36:40 +0000

    Updated app settings to allow base url without trailing slash Reset default appsetting values

--- src/TeamsTalentMgmtAppV4/Models/AppSettings.cs
+++ src/TeamsTalentMgmtAppV4/Models/AppSettings.cs
@@ -2,7 +2,10 @@
 {
     public class AppSettings
     {
-        public string BaseUrl { get; set; }
+        private string _baseUrl = "";
+
+        public string BaseUrl { get => _baseUrl; set => _baseUrl = value.TrimEnd('/') + "/"; }
+
         public string TeamsAppId { get; set; }
         public string MicrosoftAppId { get; set; }
         public string MicrosoftAppPassword { get; set; }
